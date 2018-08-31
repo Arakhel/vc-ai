fe8a1f6962e7a7cf964a350f15334c79626d5607
Nicolaus <euclides@zuragan.com>
2018-08-31 21:43:24 +0700

    Program Palang Pintu

--- src/PalangPintu.java
+++ src/PalangPintu.java
@@ -1,26 +1,29 @@
 import java.lang.reflect.Array;
+import java.util.Scanner;
 
 public class PalangPintu {
     private String orang[][];
     private int baris = 1;
     private int kolom = 10;
 
+    Scanner sc = new Scanner(System.in);
+
     public PalangPintu() {
         orang = new String[baris][kolom];
-        for (int i = 0; i < baris ; i++) {
+        for (int i = 0; i < baris; i++) {
             for (int j = 0; j < kolom; j++) {
-                orang[i][j]= " ";
+                System.out.println("Apakah ada orang? (p/-)");
+                orang[i][j] = sc.next();
+                if (orang[i][j].equals("p")) {
+                    buka();
+                } else {
+                    tutup();
+                }
             }
         }
-
-        orang[0][0] = "p";
-        orang[0][2] = "p";
-        orang[0][3] = "p";
-        orang[0][7] = "p";
-        orang[0][9] = "p";
     }
 
-    public String toString(){
+    public String toString() {
         StringBuilder r = new StringBuilder();
         for (int i = 0; i < baris; i++) {
 
@@ -32,47 +35,11 @@ public class PalangPintu {
         return r.toString();
     }
 
-    private int CountPeople() {
-        int count = 0;
-        for (int i = 0; i < baris; i++) {
-            for (int j = 0; j < kolom; j++) {
-                if (orang[i][j].equals("p")) {
-                    count++;
-                }
-            }
-        }
-        // System.out.println("the found dirt " + count+" dirts");
-        return count;
-    }
-
-    private void Move(){
-//        while (CountPeople() > 0){
-            for (int i = 0; i < baris; i++) {
-                for (int j = 0; j < kolom; j++) {
-                    if(orang[i][j].equals("p")){
-                        buka();
-                    }
-                    else {
-                        tutup();
-                    }
-                }
-            }
-//        }
-    }
-
-    private void buka(){
+    private void buka() {
         System.out.println("Buka Pintu ada orang");
     }
 
-    private void tutup(){
+    private void tutup() {
         System.out.println("Tutup Pintu baru gak ada orang");
-    }
-
-    public static void main(String[] args) {
-        PalangPintu palang = new PalangPintu();
-        System.out.println("Ceritanya pintu otomatis mall");
-        System.out.println(palang.toString());
-        palang.Move();
-        System.out.println("Sudah gak ada orang Bang");
     }
 }
---
+++ src/PalangPintuMain.java
@@ -0,0 +1,8 @@
+public class PalangPintuMain {
+    public static void main(String[] args) {
+        System.out.println("Ceritanya pintu otomatis mall");
+        PalangPintu palang = new PalangPintu();
+        System.out.println();
+        System.out.println("Sudah gak ada orang Bang");
+    }
+}
